cecd89cd74f011eac2fcdf6f5274e239fd2109da
Davide Bettio <davide.bettio@ispirata.com>
2020-07-17 17:11:55 +0200

    Import JSONTemplate code from astarte_flow
    See also:
    https://github.com/astarte-platform/astarte_flow/blob/master/lib/astarte_flow/blocks/json_path_mapper/json_template.ex

--- mix.exs
+++ mix.exs
@@ -1,3 +1,21 @@
+#
+# This file is part of ExJSONTemplate.
+#
+# Copyright 2020 Ispirata Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule ExJSONTemplate.MixProject do
   use Mix.Project
 
@@ -11,18 +29,15 @@ defmodule ExJSONTemplate.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:exjsonpath, "~> 0.9.0"},
     ]
   end
 end
--- test/ex_json_template_test.exs
+++ test/ex_json_template_test.exs
@@ -1,8 +1,87 @@
+#
+# This file is part of ExJSONTemplate.
+#
+# Copyright 2020 Ispirata Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 defmodule ExJSONTemplateTest do
   use ExUnit.Case
-  doctest ExJSONTemplate
+  alias ExJSONTemplate
 
-  test "greets the world" do
-    assert ExJSONTemplate.hello() == :world
+  test "render string literal" do
+    assert ExJSONTemplate.compile_template("foo bar") == {:ok, "foo bar"}
+  end
+
+  test "render number literal" do
+    assert ExJSONTemplate.compile_template(42) == {:ok, 42}
+  end
+
+  test "render string interpolation using a map" do
+    {:ok, compiled_template} = ExJSONTemplate.compile_template("Hello {{ $.first_name }}!")
+
+    map = %{"first_name" => "Foo", "last_name" => "Bar"}
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, "Hello Foo!"}
+  end
+
+  test "render string interpolation which begins with {{ using a map" do
+    {:ok, compiled_template} = ExJSONTemplate.compile_template("{{ $.first_name }} ")
+
+    map = %{"first_name" => "Foo", "last_name" => "Bar"}
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, "Foo "}
+  end
+
+  test "render multiple string interpolation using a map" do
+    template = "x: {{ $.x }}, y: {{ $.y }}, z: 0"
+    {:ok, compiled_template} = ExJSONTemplate.compile_template(template)
+
+    map = %{"x" => 0.5, "y" => -1.0, "t" => 5.3}
+    expected_rendered = "x: 0.5, y: -1.0, z: 0"
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, expected_rendered}
+  end
+
+  test "render placeholder using a map" do
+    {:ok, compiled_template} = ExJSONTemplate.compile_template("{{ $.data }}")
+
+    map = %{"data" => %{"1" => 1, "a" => "A"}}
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, %{"1" => 1, "a" => "A"}}
+  end
+
+  test "render map with placeholder using a map" do
+    template = %{"data" => %{"result" => "{{ $.data }}"}}
+    {:ok, compiled_template} = ExJSONTemplate.compile_template(template)
+
+    map = %{"data" => %{"1" => 1, "a" => "A"}}
+    expected_rendered = %{"data" => %{"result" => %{"1" => 1, "a" => "A"}}}
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, expected_rendered}
+  end
+
+  test "render array with placeholders using a map" do
+    template = ["{{ $.data.x }}", "{{ $.data.y }}", 0]
+    {:ok, compiled_template} = ExJSONTemplate.compile_template(template)
+
+    map = %{"data" => %{"x" => 0.5, "y" => -1.0, "t" => 5.3}}
+    expected_rendered = [0.5, -1.0, 0]
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, expected_rendered}
+  end
+
+  test "render array with string interpolation using a map" do
+    template = %{"data" => ["x: {{ $.x }}", "y: {{ $.y }}", "z: 0"]}
+    {:ok, compiled_template} = ExJSONTemplate.compile_template(template)
+
+    map = %{"x" => 0.5, "y" => -1.0, "t" => 5.3}
+    expected_rendered = %{"data" => ["x: 0.5", "y: -1.0", "z: 0"]}
+    assert ExJSONTemplate.render(compiled_template, map) == {:ok, expected_rendered}
   end
 end
